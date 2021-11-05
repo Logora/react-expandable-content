6b1b2b491475de75458143bb7173f3f7916fee93
Henry Boisgibault <h.boisgibault@gmail.com>
2021-11-05 16:15:39 +0100

    Better example

--- examples/index.js
+++ examples/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import ExpandableContent from '../src/index.js';
 
 ReactDOM.render(
-  <div>
-    <p>You rendered the React module:</p>
+  <div style={{width: "200px"}}>
     <ExpandableContent maxHeight={200}>mon contenu You rendered the React module: You rendered the React module: You rendered the React module: You rendered the React module: You rendered the React module:</ExpandableContent>
   </div>,
   document.getElementById('app')
